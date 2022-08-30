5c9e0e926fc441cc6a2cf6a80d7f81204642b994
liimmaa <halimammegwa@gmail.com>
2022-08-30 03:12:48 +0100

    Initial Commit

---
+++ RandomUsersChat/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  RandomUsersChat
+//
+//  Created by Chioma Amanda Mmegwa  on 30/08/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ RandomUsersChat/RandomUsersChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  RandomUsersChatApp.swift
+//  RandomUsersChat
+//
+//  Created by Chioma Amanda Mmegwa  on 30/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct RandomUsersChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
