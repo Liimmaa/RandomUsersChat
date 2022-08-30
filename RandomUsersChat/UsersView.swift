37874a7092fac1fe83d4e23d04091d3da81359dd
liimmaa <halimammegwa@gmail.com>
2022-08-30 04:30:23 +0100

    Display a list of users

--- RandomUsersChat/UsersView.swift
+++ RandomUsersChat/UsersView.swift
@@ -13,9 +13,8 @@ struct UsersView: View {
     var body: some View {
         NavigationView {
             VStack {
-                Text("Raw JSON Data:")
-                ScrollView{
-                    Text(userData.users)
+                List(userData.users) { user in
+                    Text(user.fullName)
                 }
             }
             .padding()
